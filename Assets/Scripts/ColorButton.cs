c77f1471a996748ce6a18f8beebe2504990b54e3
taylorjames9 <taylorjames9@gmail.com>
2016-06-23 12:31:10 -0700

    second commit

---
+++ Assets/Scripts/ColorButton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript;
+using TouchScript.Behaviors;
+using TouchScript.Gestures;
+using System;
+using unitycoder_MobilePaint;
+using UnityEngine.UI;
+
+public class ColorButton : MonoBehaviour {
+
+	public unitycoder_MobilePaint.ColorUIManager ColorManager;
+
+
+	void OnEnable(){
+		GetComponent<PressGesture> ().Pressed += ChangeToMyColor;
+	}
+
+	void OnDisable(){
+
+		GetComponent<PressGesture> ().Pressed -= ChangeToMyColor;
+
+	}
+
+	public void ChangeToMyColor(object sender, EventArgs e){
+
+
+		ColorManager.SetCurrentColor (GetComponent<Button> ());
+
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
